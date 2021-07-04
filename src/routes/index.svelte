a6d9ae320eff26cf2f8491507d26241e47affe0f
Marten <marten.frisk@gmail.com>
2021-07-05 00:32:08 +0200

    Remove unused code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,5 @@
-<script context="module" lang="ts">
-	export const prerender = true;
-	/**
-	 * @type {import('@sveltejs/kit').Load}
-	 */
-	// export async function load({ page }) {
-	// 	console.log(page.query.toString())
-		
-	// 	const qs = page.query.get('s');
-	// 	const filter = page.query.get('f');
-	// 	console.log(qs)
-		
-	// 	return {
-	// 		props: {
-	// 			query: qs || '',
-	// 			filter: filter || []
-	// 		}
-	// 	};
-	// }
-</script>
-
 <script lang="ts">
 	import Search from '$lib/Search.svelte';
-	// export let query: string, filter: any[];
 </script>
 
 <Search />
