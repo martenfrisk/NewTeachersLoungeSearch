c8a80dedb060fd1240f407a256bbc871827c0c4e
Mårten Frisk <marten.frisk@gmail.com>
2023-04-29 20:11:24 +0200

    uff

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -4,8 +4,8 @@
 	export let stats: HitStats, query: string, filter: string[];
 </script>
 
-{#if stats?.estimatedTotalHits && stats?.estimatedTotalHits > 0}
-	<p class="flex flex-wrap gap-1 my-4 text-sm md:mt-6 md:mb-8">
+<p class="flex flex-wrap gap-1 my-4 text-sm md:mt-6 md:mb-8">
+	{#if stats?.estimatedTotalHits && stats?.estimatedTotalHits > 0}
 		<span>
 			{stats.estimatedTotalHits} hits for <em>"{query}"</em>
 			{#if filter?.length > 0}
@@ -16,14 +16,10 @@
 			{/if}
 		</span>
 		<span>(results retrieved in {stats.processingTime}ms)</span>
-	</p>
-{:else if stats?.estimatedTotalHits == 0}
-	<p class="flex flex-wrap gap-1 my-4 text-sm md:mt-6 md:mb-8">
+	{:else if stats?.estimatedTotalHits == 0}
 		No results for <em>"{query}"</em>
 		<!-- {filterEdited ? '(edited lines only)' : ''} ☹ -->
-	</p>
-{:else}
-	<p class="mt-6 mb-8">
-		<span class="w-20 h-8 bg-gray-100 animate-pulse" />
-	</p>
-{/if}
+	{:else}
+		<span class="w-40 block bg-gray-100 animate-pulse">&nbsp;</span>
+	{/if}
+</p>
