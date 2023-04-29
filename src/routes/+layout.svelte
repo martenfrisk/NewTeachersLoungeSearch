9746f29e7029db741fab6c73352d192c2a471476
Mårten Frisk <marten.frisk@gmail.com>
2023-04-29 21:48:21 +0200

    readd analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import Sidebar from '$lib/Sidebar.svelte';
 	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import Search from '$lib/Search.svelte';
 	import type { SearchHit } from '$lib/types';
-	import { searchMeili, newRandom } from '$lib/utils';
-	export let data: any;
+	export let data: { query: string; filter: string[]; hits: SearchHit[] };
 
-	const { query, filter, hits }: { query: string; filter: string[]; hits: SearchHit[] } = data;
+	const { query, filter, hits } = data;
 </script>
 
 <Search {query} {filter} {hits} />
