1fb28de1befea09a2da67e476d6cb864be0a5ba2
Mårten Frisk <marten.frisk@gmail.com>
2023-04-30 14:25:11 +0200

    more functions

--- meilitools.js
+++ meilitools.js
@@ -5,8 +5,8 @@ import path from 'path';
 // import { MeiliKey } from './src/lib/Env';
 
 const client = new MeiliSearch({
-	host: 'https://ts.pcast.site/',
-	apiKey: MeiliKey
+	host: 'https://ts.pcast.site/'
+	// apiKey: MeiliKey
 });
 
 /**
@@ -72,35 +72,6 @@ function parseArguments(args) {
 	return parsedArgs;
 }
 
-const args = parseArguments(process.argv.slice(2));
-
-if (args['--gettask'] !== undefined) {
-	const taskId = parseInt(args['--gettask'], 10);
-
-	if (!isNaN(taskId)) {
-		getTask(taskId);
-	} else {
-		console.log('Error: Invalid task ID.');
-	}
-} else if (args['--import'] !== undefined) {
-	const filePath = args['--import'];
-	importDocuments(filePath);
-} else if (args['--import-all'] !== undefined) {
-	const folderPath = args['--import-all'];
-	importAllDocuments(folderPath);
-} else if (args['--fix-episode'] !== undefined) {
-	const filePath = args['--fix-episode'];
-	fixEpisodeField(filePath);
-} else if (args['--fix-all-episodes'] !== undefined) {
-	const folderPath = args['--fix-all-episodes'];
-	fixAllEpisodeFields(folderPath);
-} else {
-	console.log('Usage:');
-	console.log('  node meilitool.js --gettask=<task_id>');
-	console.log('  node meilitool.js --import=<file_path>');
-	console.log('  node meilitool.js --import-all=<folder_path>');
-}
-
 /**
  * @param {string} filePath
  * @returns {TranscriptLine[]}
@@ -134,7 +105,15 @@ function addID(dataArray) {
 				return null;
 			}
 
-			return { id: `${cleanEpisode}-${index + 1}`, episode: cleanEpisode, season, ...item };
+			return {
+				id: `${cleanEpisode}-${index + 1}`,
+				episode: cleanEpisode,
+				season,
+				time: item.time,
+				speaker: item.speaker,
+				line: item.line,
+				edited: item.edited
+			};
 		})
 		.filter((item) => item !== null);
 }
@@ -206,4 +185,78 @@ function updateFilters() {
 		.catch((error) => {
 			console.log(error);
 		});
+}
+
+/**
+ * @param {string} folderPath
+ */
+async function addIdsToFile(folderPath) {
+	const files = fs.readdirSync(folderPath);
+
+	for (const file of files) {
+		const filePath = path.join(folderPath, file);
+		const fileStats = fs.statSync(filePath);
+
+		if (fileStats.isFile() && path.extname(file) === '.json') {
+			console.log(`Adding IDs: ${file}`);
+			const data = readJSONFile(filePath);
+			const dataWithID = addID(data);
+			fs.writeFileSync(filePath, JSON.stringify(dataWithID, null, 2));
+		}
+	}
+}
+
+/**
+ * @param {string} folderPath
+ */
+async function removeEmptyLinesAndAddIds(folderPath) {
+	const files = fs.readdirSync(folderPath);
+
+	for (const file of files) {
+		const filePath = path.join(folderPath, file);
+		const fileStats = fs.statSync(filePath);
+
+		if (fileStats.isFile() && path.extname(file) === '.json') {
+			console.log(`Removing empty lines and adding IDs: ${file}`);
+			const data = readJSONFile(filePath);
+			const filteredData = data.filter((item) => item.line.trim() !== '');
+			const dataWithID = addID(filteredData);
+			fs.writeFileSync(filePath, JSON.stringify(dataWithID, null, 2));
+		}
+	}
+}
+
+const args = parseArguments(process.argv.slice(2));
+
+if (args['--gettask'] !== undefined) {
+	const taskId = parseInt(args['--gettask'], 10);
+
+	if (!isNaN(taskId)) {
+		getTask(taskId);
+	} else {
+		console.log('Error: Invalid task ID.');
+	}
+} else if (args['--import'] !== undefined) {
+	const filePath = args['--import'];
+	importDocuments(filePath);
+} else if (args['--import-all'] !== undefined) {
+	const folderPath = args['--import-all'];
+	importAllDocuments(folderPath);
+} else if (args['--fix-episode'] !== undefined) {
+	const filePath = args['--fix-episode'];
+	fixEpisodeField(filePath);
+} else if (args['--fix-all-episodes'] !== undefined) {
+	const folderPath = args['--fix-all-episodes'];
+	fixAllEpisodeFields(folderPath);
+} else if (args['--add-ids'] !== undefined) {
+	const folderPath = args['--add-ids'];
+	addIdsToFile(folderPath);
+} else if (args['--remove-empty-lines'] !== undefined) {
+	const folderPath = args['--remove-empty-lines'];
+	removeEmptyLinesAndAddIds(folderPath);
+} else {
+	console.log('Usage:');
+	console.log('  node meilitool.js --gettask=<task_id>');
+	console.log('  node meilitool.js --import=<file_path>');
+	console.log('  node meilitool.js --import-all=<folder_path>');
 }
