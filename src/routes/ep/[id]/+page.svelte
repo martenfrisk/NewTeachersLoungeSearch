63c4b961458d08dc53df2bae8cee54126f405e07
Mårten Frisk <marten.frisk@gmail.com>
2023-05-01 21:50:18 +0200

    prerender ep pages

--- src/routes/ep/[id]/+page.svelte
+++ src/routes/ep/[id]/+page.svelte
@@ -2,26 +2,28 @@
 	import type { PageData } from './$types';
 	import { onMount } from 'svelte';
 	import epList from 'assets/episodes.json';
+	import { page } from '$app/stores';
 
 	export let data: PageData;
 
-	const { episode, query } = data;
-	const epPromise = import(`../../../assets/transcripts/${episode}.json`);
+	const { episode, hits } = data;
+	let query: URLSearchParams | null = null;
 
 	function epName(episode: string) {
 		return epList.find((x) => x.ep === episode.replace('.json', ''));
 	}
+	const epScript = hits.default;
 
 	const hitIsActive = (ep: string) => {
-		if (!query.has('t')) return false;
+		if (!query?.has('t')) return false;
 		if (ep.replaceAll(':', '') === query.get('t')?.replace('t-', '')) return true;
 		return false;
 	};
 	onMount(async () => {
-		const hits = await epPromise;
-		if (hits && query.has('t')) {
+		query = $page.url?.searchParams;
+		if (epScript && query?.has('t')) {
 			setTimeout(() => {
-				const anchorId = query.get('t');
+				const anchorId = query?.get('t');
 				if (anchorId) {
 					const anchor = document.getElementById(anchorId) as HTMLElement;
 					if (!anchor) return;
@@ -46,30 +48,28 @@
 	<div class="text-lg text-gray-700 uppercase">{epName(episode)?.ep}</div>
 	<div class="text-lg text-gray-800 md:text-xl">{epName(episode)?.title}</div>
 	<p class="px-4 my-8 md:mr-10">{epName(episode)?.desc}</p>
-	{#await epPromise then hits}
-		{#each hits.default as hit}
-			<div
-				class={`w-full border-2 px-4 pb-6 mb-6 shadow-md hover:bg-blue-50 ${
-					hitIsActive(hit.time) ? 'border-blue-500' : 'border-blue-100'
-				}`}
-				id={`t-${hit.time.replaceAll(':', '')}`}
-			>
-				<div class="flex flex-wrap items-center justify-between w-full mb-2">
-					<div class="flex items-center">{hit.time}</div>
-					<div class="flex items-center font-mono text-right text-gray-600">
-						<div class="mr-2 font-sans text-black">{hit.speaker}</div>
-						&nbsp;
-						{#if hit.edited}
-							<span class="text-2xl text-green-400">✔</span>
-						{:else}
-							<span class="text-2xl text-gray-400">&minus;</span>
-						{/if}
-					</div>
-				</div>
-				<div class="py-2 pl-4 mt-4 md:text-lg">
-					<p>{hit.line}</p>
+	{#each epScript as hit}
+		<div
+			class={`w-full border-2 px-4 pb-6 mb-6 shadow-md hover:bg-blue-50 ${
+				hitIsActive(hit.time) ? 'border-blue-500' : 'border-blue-100'
+			}`}
+			id={`t-${hit.time.replaceAll(':', '')}`}
+		>
+			<div class="flex flex-wrap items-center justify-between w-full mb-2">
+				<div class="flex items-center">{hit.time}</div>
+				<div class="flex items-center font-mono text-right text-gray-600">
+					<div class="mr-2 font-sans text-black">{hit.speaker}</div>
+					&nbsp;
+					{#if hit.edited}
+						<span class="text-2xl text-green-400">✔</span>
+					{:else}
+						<span class="text-2xl text-gray-400">&minus;</span>
+					{/if}
 				</div>
 			</div>
-		{/each}
-	{/await}
+			<div class="py-2 pl-4 mt-4 md:text-lg">
+				<p>{hit.line}</p>
+			</div>
+		</div>
+	{/each}
 </div>
